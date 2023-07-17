423dd7a8fef652e0beb8fa9d067997803dbd03a1
Marvin Hsu <marvinhsu@nine-yi.com>
2023-07-17 17:35:53 +0800

    add two sum test

---
+++ problems/twosum/solution.go
@@ -0,0 +1,15 @@
+package twosum
+
+func TwoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if idx, ok := seen[complement]; ok {
+			return []int{idx, i}
+		}
+		seen[num] = i
+	}
+
+	return nil
+}
